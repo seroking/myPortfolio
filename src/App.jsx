617a5800ea12516ab21f90ea17f53ba9853a5bfa
seroking <baridmohamedamine004@gmail.com>
2025-02-22 23:37:41 +0100

    first late commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+import { LoadingScreen } from './components/LoadingScreen'
+import './index.css'
+import {Navbar} from './components/Navbar';
+import { MobileMenu } from './components/MobileMenu';
+import { Home } from './components/sections/Home';
+import { About } from './components/sections/About';
+function App() {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <>
+      {!isLoading && <LoadingScreen onComplete={()=> setIsLoading(true)}/>}
+
+        <div className={`min-h-screen transition-opacity duration-700 ${isLoading ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+          <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+          <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+          <Home/>
+          <About/>
+        </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/sections/About.jsx
@@ -0,0 +1,63 @@
+export const About = () => {
+    const frontendSkills = ["React", "TailwindCSS", "Javascript"]
+    const backendSkills = ["MongoDB", "Python", "PHP", "Laravel"]
+
+    return (
+        <section id="about" className="min-h-screen flex items-center justify-center py-20">
+            <div className="max-w-3xl mx-auto px-4">
+                <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center"> About Me</h2>
+                <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all ">
+                    <p className="text-gray-300 mb-6 ">
+                        I'm Sero, a web developer passionate about creating clean and engaging digital experiences.
+                        I love learning, problem-solving, and bringing ideas to life.
+                    </p>
+
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all ">
+                            <h3 className="text-xl font-bold mb-4">Frontend</h3>
+                            <div className="flex flex-wrap gap-2">
+                                {frontendSkills.map((skill, key) => {
+                                    return (
+                                        <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2_8_rgba(59,130,246,0.2)] transition">
+                                            {skill}
+                                        </span>)
+                                })}
+                            </div>
+                        </div>
+
+                        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all ">
+                            <h3 className="text-xl font-bold mb-4">Backend</h3>
+                            <div className="flex flex-wrap gap-2">
+                                {backendSkills.map((skill, key) => {
+                                    return (
+                                        <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2_8_rgba(59,130,246,0.2)] transition">
+                                            {skill}
+                                        </span>)
+                                })}
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+                    <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+                        <h3 className="text-xl font-bold mb-4">
+                            🏫Education
+                        </h3>
+                        <ul className="list-disc list-inside text-gray-300 space-y-2">
+                            <li><strong>Technicien Specialise en developement web full stack</strong> - ISTA EL HANK (2023-2025)</li>
+                        </ul>
+                    </div>
+                    <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+                        <h3 className="text-xl font-bold mb-4">
+                            💼Work
+                        </h3>
+                    </div>
+                </div>
+            </div>
+
+
+        </section>
+    )
+}
