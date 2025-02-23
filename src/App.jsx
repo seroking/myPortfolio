f0490a27a750fb263cc0a51a0a3c7871141cbc29
seroking <baridmohamedamine004@gmail.com>
2025-02-23 14:03:00 +0000

    finishing all sections

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import {Navbar} from './components/Navbar';
 import { MobileMenu } from './components/MobileMenu';
 import { Home } from './components/sections/Home';
 import { About } from './components/sections/About';
+import { Projects } from './components/sections/Projects';
+import { Contact } from './components/sections/Contact';
 function App() {
 
   const [isLoading, setIsLoading] = useState(false);
@@ -20,6 +22,8 @@ function App() {
           <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
           <Home/>
           <About/>
+          <Projects/>
+          <Contact/>
         </div>
     </>
   )
--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,9 +1,12 @@
+import { RevealOnScroll } from "./RevealOnScroll"
+
 export const About = () => {
     const frontendSkills = ["React", "TailwindCSS", "Javascript"]
     const backendSkills = ["MongoDB", "Python", "PHP", "Laravel"]
 
     return (
         <section id="about" className="min-h-screen flex items-center justify-center py-20">
+            <RevealOnScroll>
             <div className="max-w-3xl mx-auto px-4">
                 <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center"> About Me</h2>
                 <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all ">
@@ -30,7 +33,7 @@ export const About = () => {
                             <div className="flex flex-wrap gap-2">
                                 {backendSkills.map((skill, key) => {
                                     return (
-                                        <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2_8_rgba(59,130,246,0.2)] transition">
+                                        <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2_8_rgba(59,130,246,0.1)] transition-all">
                                             {skill}
                                         </span>)
                                 })}
@@ -51,13 +54,23 @@ export const About = () => {
                     </div>
                     <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
                         <h3 className="text-xl font-bold mb-4">
-                            💼Work
+                            💼Work Experience
                         </h3>
+                        <div className="pace-y-4 text-gray-300">
+                            <div>
+                                <h4 className="font-semi-bold">teleconseiller a ELMI </h4>
+                                <p>was a pretty bad job but i needed money and experience</p>
+                            </div>
+                            <div >
+                                <h4 className="font-semi-bold"> no job  </h4>
+                                <p>always at home </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
 
-
+            </RevealOnScroll>
         </section>
     )
 }
